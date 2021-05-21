4671a53969deb90acf19ad79b3396d79af4251d0
Samuel2779 <44899739+Samuel2779@users.noreply.github.com>
2021-05-20 19:21:21 -0500

    connect to database cluster

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config();
+
 const port = process.env.PORT || 3000;
+
+const mongoose = require('mongoose');
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.5f0qa.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, 
+    {useNewUrlParser: true, useUnifiedTopology: true}
+)
+    .then(() => console.log('DB connected'))
+    .catch(e => console.log(e))
+
 
 app.get('/', (req, res) => {
     res.send('Express try')
